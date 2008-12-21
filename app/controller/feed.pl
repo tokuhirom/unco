9026483c78788a3be942b3661dc64941c823c250
Tokuhiro Matsuno <tokuhirom@gmail.com>
2008-12-21 18:45:01 +0900

    oops

--- app/controller/feed.pl
+++ app/controller/feed.pl
@@ -4,7 +4,7 @@ sub run {
     # XXX このへんの設定がもうちょいすっきりかけるといいね
     sql_dbh(@{ config()->{application}->{sql_dsn} });
     my $feed_id = param('feed_id') or die "missing feed_id";
-    my $feed = sql_select_one('SELECT * FROM entry WHERE id=?', $feed_id);
+    my $feed = sql_select_one('SELECT * FROM feed WHERE id=?', $feed_id);
     my ($entries, $pager) = sql_select_paginate('SELECT * FROM entry WHERE feed=? ORDER BY id DESC', [$feed_id], {rows => 10, page => param('page') || 1});
 
     render_and_print(
