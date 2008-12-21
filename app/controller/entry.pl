c05f86f3c25432f2c1799a4020310186630ad13c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2008-12-21 20:23:49 +0900

    display more text

--- app/controller/entry.pl
+++ app/controller/entry.pl
@@ -14,7 +14,7 @@ sub run {
     ($entry->{body}, my $pager) = sub {
         my $body = shift;
         my $html = $extractor->extract( $body )->as_html;
-        my @pages = HTML::Split->split(html => $html, length => 1024);
+        my @pages = HTML::Split->split(html => $html, length => 2048);
         my $page = param('page')||1;
         my $ret = Text::MicroTemplate::encoded_string($pages[$page - 1]);
         my $pager = Data::Page->new(scalar(@pages), 1, $page);
