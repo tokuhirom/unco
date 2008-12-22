f6133bfcf5dc5b124407199074527f66042d4e8a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2008-12-22 19:12:57 +0900

    EFT きいてないのをさがしやすくした

--- app/controller/feed.pl
+++ app/controller/feed.pl
@@ -5,7 +5,8 @@ sub run {
     sql_dbh(@{ config()->{application}->{sql_dsn} });
     my $feed_id = param('feed_id') or die "missing feed_id";
     my $feed = sql_select_one('SELECT * FROM feed WHERE id=?', $feed_id);
-    my ($pages, $pager) = sql_select_paginate('SELECT page.id, page.title, page.hatena_bookmark_count FROM page INNER JOIN entry ON (page.id=entry.page) WHERE entry.feed=? ORDER BY entry.id DESC', [$feed->{id}], {rows => 20, page => param('page')||1});
+    # page.body はデバッグ用にだしてる
+    my ($pages, $pager) = sql_select_paginate('SELECT page.id, page.title, page.hatena_bookmark_count, page.body FROM page INNER JOIN entry ON (page.id=entry.page) WHERE entry.feed=? ORDER BY entry.id DESC', [$feed->{id}], {rows => 20, page => param('page')||1});
 
     render_and_print(
         'tmpl/feed.mt',
